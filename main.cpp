d038d6b4d635c88c445379ab2b300c30221dea44
Andrey <86359412+Santas7@users.noreply.github.com>
2023-03-13 12:52:12 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,119 +5,176 @@
 
 #include <iostream>
 
-class Set { 
+class Set {
 private:
     struct TreeNode {
-        int data;
+        int value;
         TreeNode* left;
         TreeNode* right;
-        TreeNode(int x) : data(x), left(nullptr), right(nullptr) {}
+        TreeNode(int value) : value(value), left(nullptr), right(nullptr) {}
     };
-    TreeNode* root = nullptr;
+    TreeNode* _root;
 
-public:
-    // конструктор
-    Set() = default;
-
-    // конструктор копирования
-    Set(const Set& obj) = default;
-
-    // деструктор
-    ~Set() = default;
-
-    TreeNode* get_root() const { return root; }
-
-    // оператор присваивания
-    /*
-    * data - array datas
-    Set& operator=(const Set& other) {
-        if (this != &other) {
-            data.clear();
-            for (int num : other.data) {
-                data.push_back(num);
-            }
-        }
-        return *this;
-    }
-    */
-
-    // метод для вставки элемента
-    TreeNode* insert(TreeNode* root, int value) {
-        if (root == nullptr) 
-            return new TreeNode(value);
-        if (value < root->data)
-            root->left = insert(root->left, value);
-        else if (value > root->data)
-            root->right = insert(root->right, value);
-        return root;
-    }
-    // метод удаления элемента из дерева
-    TreeNode* erase(TreeNode* root, int value) {
-        if (root == nullptr) 
-            return nullptr;
-        if (value < root->data)
-            root->left = erase(root->left, value);
-        else if (value > root->data)
-            root->right = erase(root->right, value);
- 
-        else {
-            if (root->left == nullptr) {
-                TreeNode* temp = root->right;
-                delete root;
-                return temp;
-            }
-            else if (root->right == nullptr) {
-                TreeNode* temp = root->left;
-                delete root;
-                return temp;
-            }
-            TreeNode* temp = root->right;
-            while (temp->left != nullptr) {
-                temp = temp->left;
-            }
-            root->data = temp->data;
-            root->right = erase(root->right, temp->data);
-        }
-        return root;
-    }
-
-    // метод проверки наличия элемента
-    bool contains(TreeNode* root, int value) {
-        if (root == nullptr) 
-            return false;
-        if (value == root->data)
-            return true;
-        else if (value < root->data)
-            return contains(root->left, value);
-        else 
-            return contains(root->right, value);
-    }
-
-    // очистка дерева
+    // рекурсивная функция для удаления всех узлов дерева
     void clear(TreeNode* root) {
-        if (root != nullptr) {
+        if (root) {
             clear(root->left);
             clear(root->right);
             delete root;
         }
     }
-    
-    // метод вывода дерева в консоль
-    void print(TreeNode* root) {
-        if (root == nullptr) 
-            return;
-        std::cout << root->data << " ";
-        print(root->left);
-        print(root->right);
+
+    // рекурсивная функция для копирования дерева
+    TreeNode* copy_tree(TreeNode* root) {
+        if (!root) 
+            return nullptr; 
+
+        TreeNode* copy_node = new TreeNode(root->value);
+        copy_node->left = copy_tree(root->left);
+        copy_node->right = copy_tree(root->right);
+        return copy_node;
+    }
+
+    // рекурсивная функция для вставки узла в дерево
+    bool insert_(TreeNode*& root, int key) {
+        if (!root) {
+            root = new TreeNode(key);
+            return true;
+        }
+        if (root->value == key) 
+            return false;
+        
+        if (root->value > key) 
+            return insert_(root->left, key);
+  
+        else 
+            return insert_(root->right, key);
+    }
+
+    // рекурсивная функция для поиска узла в дереве
+    bool contains_(TreeNode* root, int key) const {
+        if (!root) 
+            return false;
+
+        if (root->value == key) 
+            return true;
+
+        if (root->value > key)  
+            return contains_(root->left, key);
+
+        else  
+            return contains_(root->right, key);
+    }
+
+    // рекурсивная функция для удаления узла из дерева
+    bool erase_(TreeNode*& root, int key) {
+        if (!root) 
+            return false;
+
+        if (root->value == key) {
+            if (!root->left) {
+                TreeNode* right_child = root->right;
+                delete root;
+                root = right_child;
+            }
+            else if (!root->right) {
+                TreeNode* left_child = root->left;
+                delete root;
+                root = left_child;
+            }
+            else {
+                TreeNode* min_right_node = root->right;
+                while (min_right_node->left) {
+                    min_right_node = min_right_node->left;
+                }
+                root->value = min_right_node->value;
+                erase_(root->right, min_right_node->value);
+            }
+            return true;
+        }
+        if (root->value > key) {
+            return erase_(root->left, key);
+        }
+        else {
+            return erase_(root->right, key);
+        }
+    }
+
+    // рекурсивная функция для печати содержимого дерева
+    void print_(TreeNode* root) const {
+        if (!root) return;
+        
+        print_(root->left);
+        std::cout << root->value << " ";
+        print_(root->right);
+    }
+
+    Set& operator=(const Set& tree) {
+        if (this != &tree) {
+            // Очищаем текущее дерево
+            clear(tree._root);
+
+            // Копируем корень дерева
+            if (tree._root != nullptr) {
+                _root = new TreeNode(*tree._root);
+            }
+
+            // Копируем остальные узлы дерева
+            copy_tree(tree._root);
+        }
+
+        return *this;
+    }
+
+public:
+    Set() : _root(nullptr) {}
+    Set(const Set& tree) : _root(nullptr) {
+        _root = copy_tree(tree._root);
+    }
+    ~Set() { clear(_root); }
+
+    bool insert(const int& key) {
+        return insert_(_root, key);
+    }
+
+    void print() const {
+        print_(_root);
+    }
+
+    bool erase(const int& key) {
+        return erase_(_root, key);
+    }
+
+    bool contains(const int& key) {
+        return contains_(_root, key);
     }
 };
 
-int main() {
-    Set tree1;
-    tree1.insert(tree1.get_root(), 1);
-    tree1.insert(tree1.get_root(), 2);
-    tree1.insert(tree1.get_root(), 3);
+int main()
+{
+    Set tree;
+    // пример работы кода
+    // заполняем дерево
+    tree.insert(4);
+    tree.insert(5);
+    tree.insert(2);
+    tree.insert(1);
+    tree.insert(6);
 
-    tree1.print(tree1.get_root());
-	return 0;
+    // вывод всего дерева в консоль
+    tree.print();
+
+    // проверка наличия элемента
+    std::cout << tree.contains(2) << std::endl;
+    std::cout << tree.contains(8) << std::endl;
+    std::cout << "\n+1\n";
+    tree.print();
+
+    // удаление элемента
+    tree.erase(2);
+    tree.erase(8);
+    std::cout << "\n+2\n";
+    tree.print();
+
+    return 0;
 }
