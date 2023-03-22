433f0156283913a01141a4b4f687eee4f16063d7
Andrey <86359412+Santas7@users.noreply.github.com>
2023-03-22 18:42:58 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,35 +2,45 @@
 * Laboratory work number 1, option 14(2)
 * Panyshkin Andrei Mikhailovich, group 6213-100503D 
 */
-
 #include <iostream>
 #include "Set.h"
 
 int main()
 {
-    Set tree;
+    Set set_1, set_2;
     // пример работы кода
     // заполняем дерево
-    tree.insert(4);
-    tree.insert(5);
-    tree.insert(2);
-    tree.insert(1);
-    tree.insert(6);
+    set_1.insert(4);
+    set_1.insert(5);
+    set_1.insert(2);
+    set_1.insert(1);
+    set_1.insert(6);
 
+    set_2.insert(5);
+    set_2.insert(3);
+    set_2.insert(6);
     // вывод всего дерева в консоль
-    tree.print();
+    set_1.print();
+    set_2.print();
 
     // проверка наличия элемента
-    std::cout << tree.contains(2) << std::endl;
-    std::cout << tree.contains(8) << std::endl;
+    std::cout << set_1.contains(2) << std::endl;
+    std::cout << set_1.contains(8) << std::endl;
     std::cout << "\n+1\n";
-    tree.print();
+    set_1.print();
 
     // удаление элемента
-    tree.erase(2);
-    tree.erase(8);
+    set_1.erase(2);
+    set_1.erase(8);
     std::cout << "\n+2\n";
-    tree.print();
+    set_1.print();
 
+    // объединение множеств
+    Set set_3 = Set().union_set(set_1, set_2);
+    set_3.print();
+
+    // cимметрическая разность двух множеств
+    Set set_3 = Set().symmetric_difference(set_1, set_2);
+    set_3.print();
     return 0;
 }
