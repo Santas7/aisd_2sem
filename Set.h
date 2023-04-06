d233fe8d77d9a583e3e9635b7bda8d8fc11057ce
Andrey <86359412+Santas7@users.noreply.github.com>
2023-04-06 16:32:58 +0400

    Update Set.h

--- Set.h
+++ Set.h
@@ -1,32 +1,33 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Set {
 private:
     struct TreeNode {
-        int value;
-        TreeNode* left;
-        TreeNode* right;
+        int value; // значение узла
+        TreeNode* left; // левый потомок
+        TreeNode* right; // правый потомок
         TreeNode(int value) : value(value), left(nullptr), right(nullptr) {}
     };
-    TreeNode* _root;
+    TreeNode* _root; // корень дерева
     // рекурсивный метод для удаления всех узлов дерева
     void clear(TreeNode* root);
     // рекурсивный метод для копирования дерева
-    TreeNode* copy_tree(TreeNode* root);
+    static TreeNode* copy_tree(const TreeNode* root);
     // рекурсивный метод для вставки узла в дерево
-    std::pair<TreeNode*, bool> insert_(TreeNode*& root, int value);
+    static std::pair<TreeNode*, bool> insert_(TreeNode*& root, int value);
     // рекурсивный метод для поиска узла в дереве
-    bool contains_(TreeNode* root, int value) const;
+    static bool contains_(const TreeNode* root, int value);
     // рекурсивный метод для удаления узла из дерева
-    bool erase_(TreeNode*& root, int value);
+    static bool erase_(TreeNode*& root, int value);
     // рекурсивный метод для печати содержимого дерева
-    void print_(TreeNode* root) const;
+    static void print_(const TreeNode* root);
     // оператор присваивания
     Set& operator=(const Set& tree);
-    void get_vector_(TreeNode* root, std::vector<int>& vector);
-
+    // рекурсивный метод для получения вектора из дерева
+    void get_vector_(TreeNode* root, std::vector<int>& vector) const;
 public:
     // конструктор
     Set();
@@ -41,7 +42,7 @@ public:
     // удаление узла из дерева
     bool erase(const int& value);
     // поиск узла в дереве
-    bool contains(const int& value);
-    // формирование вектора из дерева
-    void get_vector(std::vector<int>& vector);
+    bool contains(const int& value) const;
+    // получение вектора из дерева
+    void get_vector(std::vector<int>& vector) const;
 };
