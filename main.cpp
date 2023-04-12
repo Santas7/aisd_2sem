1e71573f7816f0d0e26fe8f1346f4d182970a814
Andrey <86359412+Santas7@users.noreply.github.com>
2023-04-12 20:30:45 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,162 +1,299 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 
 struct Stats {
     size_t comparison_count = 0;
     size_t copy_count = 0;
 };
 
-// сортировка вставками (O(n^2))
-Stats intertion_sort(std::vector<int>& vec) {
+class Sorter {
+public:
+    virtual Stats sort(std::vector<int>& vec) = 0;
     Stats stats;
-    for (int i = 1; i < vec.size(); ++i) {
-        for (int j = i; j > 0 && vec[j - 1] > vec[j]; --j) {
-            std::swap(vec[j - 1], vec[j]);
-            ++stats.copy_count;
-            ++stats.comparison_count;
-        }
-    }
-}
+};
 
-// шейкерная сортировка (O(n^2))
-Stats shaker_sort(std::vector<int>& vec) {
-    /*
-     Шейкерная сортировка представляет собой двунаправленную пузырьковую сортировку.
-     Идем по массиву сначала слева направо, перемещая наибольший элемент в конец массива, а затем справа налево,
-     перемещая наименьший элемент в начало массива.
-     */
-    Stats stats;
-    int left = 0; // левая граница
-    int right = vec.size() - 1; // правая граница
-    while (left <= right) { // пока левая граница не сомкнется с правой
-        for (int i = left; i < right; ++i) {
-            if (vec[i] > vec[i + 1]) {
-                std::swap(vec[i], vec[i + 1]);
+// сортировка вставками
+class InsertionSorter : public Sorter {
+public:
+    InsertionSorter() = default;
+    Stats sort(std::vector<int>& vec) override {
+        for (int i = 1; i < vec.size(); ++i) {
+            int j = i;
+            while (j > 0 && vec[j - 1] > vec[j]) {
+                std::swap(vec[j - 1], vec[j]);
                 ++stats.copy_count;
+                ++stats.comparison_count;
+                --j;
             }
             ++stats.comparison_count;
         }
-        --right; // сдвигаем правую границу
-        for (int i = right; i > left; --i) {
-            if (vec[i - 1] > vec[i]) {
-                std::swap(vec[i - 1], vec[i]);
-                ++stats.copy_count;
-            }
-            ++stats.comparison_count;
-        }
-        ++left; // сдвигаем левую границу
+        return stats;
     }
+};
+// шейкерная сортировка
+class ShakerSorter : public Sorter {
+public:
+    ShakerSorter() = default;
+    Stats sort(std::vector<int>& vec) override {
+        int left = 0;
+        int right = vec.size() - 1;
+        while (left <= right) {
+            for (int i = left; i < right; ++i) {
+                if (vec[i] > vec[i + 1]) {
+                    std::swap(vec[i], vec[i + 1]);
+                    ++stats.copy_count;
+                }
+                ++stats.comparison_count;
+            }
+            --right;
+            for (int i = right; i > left; --i) {
+                if (vec[i - 1] > vec[i]) {
+                    std::swap(vec[i - 1], vec[i]);
+                    ++stats.copy_count;
+                }
+                ++stats.comparison_count;
+            }
+            ++left;
+        }
+        return stats;
+    }
+};
+// сортировка пузырьком
+class BubbleSorter : public Sorter {
+public:
+    BubbleSorter() = default;
+    Stats sort(std::vector<int>& vec) override {
+        for (int i = 0; i < vec.size(); ++i) {
+            for (int j = 0; j < vec.size() - i - 1; ++j) {
+                if (vec[j] > vec[j + 1]) {
+                    std::swap(vec[j], vec[j + 1]);
+                    ++stats.copy_count;
+                }
+                ++stats.comparison_count;
+            }
+        }
+        return stats;
+    }
+};
+
+// генератор случайных чисел
+int lcg() {
+    static int x = 0;
+    x = (1021 * x + 24631) % 116640;
+    return x;
 }
 
 // заполнение вектора
-void fill_vector(std::vector<int>& vec, int size) {
-    for (int i = 0; i < size; ++i) {
-        int value = 0;
-        std::cout << "Enter value: ";
-        std::cin >> value;
-        vec.push_back(value);
+void fill_vector(std::vector<int>& vector) {
+    for (size_t i = 0; i < vector.size(); ++i) {
+        vector[i] = lcg();
     }
 }
 
 // вывод вектора
-void output_vector(std::vector<int>& vec) {
-    for (int i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << " ";
+void output_vector(std::vector<int>& vector) {
+    for (const auto& item : vector)
+        std::cout << item << " ";
     std::cout << std::endl;
 }
+// тестирование
+void testing_code_1(){
+    std::vector<int> vector(0);
+    //int size = 1000;
+    int size2= 0;
+    unsigned long long count_comp = 0;
+    unsigned long long count_copy = 0;
+
+    std::vector<unsigned long long> comp;
+    std::vector<unsigned long long> copy;
+
+    // choise panel sort
+    InsertionSorter insertion_sorter;
+    ShakerSorter shaker_sorter;
+    BubbleSorter bubble_sorter;
+
+    while (true){
+        std::cout << "[1] - Сортировка вставками\n"
+                  << "[2] - Шейкерная сортировка\n"
+                  << "[3] - Сортировка пузырьком\n"
+                  << "[4] - Назад в главное меню\n--> ";
+        int command = 0;
+        std::cin >> command;
+        switch (command){
+            case 1:
+                for (int size = 1000; size <= 100000; size += 1000){
+                    vector.resize(size);
+                    for (size_t j = 0; j < 100; ++j) {
+                        for (size_t i = 0; i < vector.size(); ++i) {
+                            vector[i] = lcg();
+                        }
+                        insertion_sorter.sort(vector);
+                        count_comp += insertion_sorter.stats.comparison_count;
+                        count_copy += insertion_sorter.stats.copy_count;
+                    }
+                    comp.push_back(count_comp / 100);
+                    copy.push_back(count_copy / 100);
+                    count_comp = 0;
+                    count_copy = 0;
+                    std::cout << "Пройден успешно " << size << std::endl;
+                    vector.clear();
+                }
+                std::cout << "Среднее количество сравнений" << std::endl;
+                for (const auto& item : comp)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                std::cout << "Среднее количество копирований" << std::endl;
+                for (const auto& item : copy)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                comp.clear();
+                copy.clear();
+                break;
+            case 2:
+                for (int size = 1000; size <= 100000; size += 1000){
+                    vector.resize(size);
+                    for (size_t j = 0; j < 100; ++j) {
+                        for (size_t i = 0; i < vector.size(); ++i) {
+                            vector[i] = lcg();
+                        }
+                        shaker_sorter.sort(vector);
+                        count_comp += shaker_sorter.stats.comparison_count;
+                        count_copy += shaker_sorter.stats.copy_count;
+                    }
+                    comp.push_back(count_comp / 100);
+                    copy.push_back(count_copy / 100);
+                    count_comp = 0;
+                    count_copy = 0;
+                    std::cout << "Пройден успешно " << size << std::endl;
+                    vector.clear();
+                }
+                std::cout << "Среднее количество сравнений" << std::endl;
+                for (const auto& item : comp)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                std::cout << "Среднее количество копирований" << std::endl;
+                for (const auto& item : copy)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                comp.clear();
+                copy.clear();
+                break;
+            case 3:
+                for (int size = 1000; size <= 100000; size += 1000){
+                    vector.resize(size);
+                    for (size_t j = 0; j < 100; ++j) {
+                        for (size_t i = 0; i < vector.size(); ++i) {
+                            vector[i] = lcg();
+                        }
+                        bubble_sorter.sort(vector);
+                        count_comp += bubble_sorter.stats.comparison_count;
+                        count_copy += bubble_sorter.stats.copy_count;
+                    }
+                    comp.push_back(count_comp / 100);
+                    copy.push_back(count_copy / 100);
+                    count_comp = 0;
+                    count_copy = 0;
+                    std::cout << "Пройден успешно " << size << std::endl;
+                    vector.clear();
+                }
+                std::cout << "Среднее количество сравнений" << std::endl;
+                for (const auto& item : comp)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                std::cout << "Среднее количество копирований" << std::endl;
+                for (const auto& item : copy)
+                    std::cout << item << " ";
+                std::cout << std::endl;
+                comp.clear();
+                copy.clear();
+                break;
+            case 4:
+                return;
+            default:
+                std::cout << "Неверная команда" << std::endl;
+                break;
+        }
+    }
+}
+
+
+void testing_code_2(){return;}
+void testing_code_3(){return;}
+
+
 
 int main() {
     // 14(10) -> 112(3) вариант задания
-    std::vector<int> vec1, vec2;
+    std::vector<int> vector(0);
     int size = 0;
+    int command2 = 0;
     // menu for user switch case
+    InsertionSorter insertion_sorter;
+    ShakerSorter shaker_sorter;
+    BubbleSorter bubble_sorter;
+
     while (true){
-        std::cout << "[1] - Заполнение вектора\n[2] - Вывод вектора\n[3] - Сортировка вставками\n[4] - Шейкерная сортировка\n[5] - Тестирование\n--> ";
-        int command;
+        std::cout << "[1] - Создать случайный вектор\n"
+                  << "[2] - Вывести вектор\n"
+                  << "[3] - Сортировка вставками\n"
+                  << "[4] - Шейкерная сортировка\n"
+                  << "[5] - Сортировка пузырьком\n"
+                  << "[6] - Тестирование\n"
+                  << "[7] - Выход\n--> ";
+        int command = 0;
         std::cin >> command;
-        switch (command) {
+        switch (command){
             case 1:
-                std::cout << "[1] - Вектор 1\n[2] - Вектор 2\n[3] Назад\n--> ";
-                int command2;
+                std::cout << "размер: ";
+                std::cin >> size;
+                vector.resize(size);
+                fill_vector(vector);
+                break;
+            case 2:
+                output_vector(vector);
+                break;
+            case 3:
+                insertion_sorter.sort(vector);
+                std::cout << "Количество сравнений: " << insertion_sorter.stats.comparison_count << std::endl;
+                std::cout << "Количество копирований: " << insertion_sorter.stats.copy_count << std::endl;
+                break;
+            case 4:
+                shaker_sorter.sort(vector);
+                std::cout << "Количество сравнений: " << shaker_sorter.stats.comparison_count << std::endl;
+                std::cout << "Количество копирований: " << shaker_sorter.stats.copy_count << std::endl;
+                break;
+            case 5:
+                bubble_sorter.sort(vector);
+                std::cout << "Количество сравнений: " << bubble_sorter.stats.comparison_count << std::endl;
+                std::cout << "Количество копирований: " << bubble_sorter.stats.copy_count << std::endl;
+                break;
+            case 6:
+                std::cout << "[1] - сред.число сравнений и копирований для 100 массивов из N элементов\n"
+                          << "[2] - сред.число сравнений и копирований для полностью отсортированного массива\n"
+                          << "[3] - сред.число сравнений и копирований для обратно отсортированного массива\n"
+                          << "[4] - Назад в главное меню\n--> ";
                 std::cin >> command2;
                 switch (command2) {
                     case 1:
-                        std::cout << "Размер вектора: ";
-                        std::cin >> size;
-                        fill_vector(vec1, size);
+                        testing_code_1();
                         break;
                     case 2:
-                        std::cout << "Размер вектора: ";
-                        std::cin >> size;
-                        fill_vector(vec2, size);
+                        testing_code_2();
                         break;
                     case 3:
+                        testing_code_3();
+                        break;
+                    case 4:
                         break;
                     default:
-                        std::cout << "Error command\n";
-                        break;
+                        std::cout << "Неверная команда!" << std::endl;
                 }
                 break;
-            case 2:
-                std::cout << "[1] - Вектор 1\n[2] - Вектор 2\n[3] Назад\n--> ";
-                int command3;
-                std::cin >> command3;
-                switch (command3) {
-                    case 1:
-                        output_vector(vec1);
-                        break;
-                    case 2:
-                        output_vector(vec2);
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        std::cout << "Error command\n";
-                        break;
-                }
-                break;
-            case 3:
-                std::cout << "[1] - Вектор 1\n[2] - Вектор 2\n[3] Назад\n--> ";
-                int command4;
-                std::cin >> command4;
-                switch (command4) {
-                    case 1:
-                        intertion_sort(vec1);
-                        break;
-                    case 2:
-                        intertion_sort(vec2);
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        std::cout << "Error command\n";
-                        break;
-                }
-                break;
-            case 4:
-                std::cout << "[1] - Вектор 1\n[2] - Вектор 2\n[3] Назад\n--> ";
-                int command5;
-                std::cin >> command5;
-                switch (command5) {
-                    case 1:
-                        shaker_sort(vec1);
-                        break;
-                    case 2:
-                        shaker_sort(vec2);
-                        break;
-                    case 3:
-                        break;
-                    default:
-                        std::cout << "Error command\n";
-                        break;
-                }
-                break;
-            case 5:
-                // тестирование
-                break;
+            case 7:
+                return 0;
             default:
-                std::cout << "Error command\n";
-                break;
+                std::cout << "Неверная команда!" << std::endl;
         }
     }
     return 0;
